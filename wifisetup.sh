3178bc1ac2bae00907c695d4cebd425b2ef7ab65
Anton Nikitenko <ant.nikitenko@gmail.com>
2020-01-19 19:33:30 -0800

    Modified process to boot with wifi and clone

--- wifisetup.sh
+++ wifisetup.sh
@@ -1,1 +1,2 @@
 sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
--- wifisetup_realprogrammersusevim.sh
+++ wifisetup_realprogrammersusevim.sh
@@ -1,1 +1,2 @@
 sudo vim /etc/wpa_supplicant/wpa_supplicant.conf
+wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
